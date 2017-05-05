5b64f4a2cac0e071486bf810965125c0fc149c55
Harry Ferrier <f3rr13r@hotmail.com>
2017-05-05 23:10:57 +0100

    Make underlines work

--- src/app/_layout/header/header.component.ts
+++ src/app/_layout/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 // routing
 import { Router } from '@angular/router';
@@ -12,23 +12,49 @@ import { HeaderContactComponent } from './header-contact/header-contact.componen
     styleUrls: [ 'header.component.scss' ]
 })
 
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+    isHomeSelected: boolean;
+    isServicesSelected: boolean;
+    isTeamSelected: boolean;
+    isFindUsSelected: boolean;
 
     constructor(private _router: Router) { }
 
+    ngOnInit() {
+        // show home page by default.
+        this.navigateToHome();
+    }
+
     navigateToHome() {
         this._router.navigateByUrl('home');
+        this.isHomeSelected = true;
+        this.isServicesSelected = false;
+        this.isTeamSelected = false;
+        this.isFindUsSelected = false;
     }
 
     navigateToServices() {
         this._router.navigateByUrl('services');
+        this.isHomeSelected = false;
+        this.isServicesSelected = true;
+        this.isTeamSelected = false;
+        this.isFindUsSelected = false;
     }
 
     navigateToTeam() {
         this._router.navigateByUrl('team');
+        this.isHomeSelected = false;
+        this.isServicesSelected = false;
+        this.isTeamSelected = true;
+        this.isFindUsSelected = false;
     }
 
     navigateToContact() {
         this._router.navigateByUrl('contact');
+        this.isHomeSelected = false;
+        this.isServicesSelected = false;
+        this.isTeamSelected = false;
+        this.isFindUsSelected = true;
     }
 }
