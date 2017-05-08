e6fc583f44fdb3589fc0a36ca20b1f365b760b17
Harry Ferrier <f3rr13r@hotmail.com>
2017-05-08 20:34:46 +0000

    Wheretofindus

--- src/app/_pages/home/home.component.ts
+++ src/app/_pages/home/home.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+// routing
+import { Router } from '@angular/router';
+
+import { GoogleMapsComponent } from '../../_shared/google-maps/google-maps.component';
+
 @Component({
     selector: 'home',
     templateUrl: 'home.component.html',
@@ -8,7 +13,17 @@ import { Component } from '@angular/core';
 
 export class HomeComponent {
 
-    buttonClicked() {
-        console.log("Button is clicked");
+    constructor(private _router: Router) { }
+
+    navigateToServices() {
+        this._router.navigateByUrl('services');
+    }
+
+    navigateToTeam() {
+        this._router.navigateByUrl('team');
+    }
+
+    navigateToFindUs() {
+        this._router.navigateByUrl('contact');
     }
 }
