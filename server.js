6da9d5ee3de7a232a6dd8fb49bcc2ae27fe3814a
Lucy Murray <lucyamurray@me.com>
2017-07-10 17:29:26 +0100

    server

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-var express = require('express');
-var app     = express();
-
-app.set('port', (process.env.PORT || 5000));
-
-//For avoidong Heroku $PORT error
-app.get('/', function(request, response) {
-    var result = 'App is running'
-    response.send(result);
-}).listen(app.get('port'), function() {
-    console.log('App is running, server is listening on port ', app.get('port'));
-});
+const express = require('express');
+const app     = express();
+const path = require('path');
 
 app.use(express.static(__dirname + '/dist'));
+
+app.listen(process.env.PORT || 8080);
+
+//PathLocationStragety
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+})
+
+console.log('8080 is listening!')
