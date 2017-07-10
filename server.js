6ff5ac42cecb11f5bf2f17ef6132a4eac49e6af1
Lucy Murray <lucyamurray@me.com>
2017-07-10 16:38:47 +0100

    server.js new

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
-const express = require('express')
-const app = express()
+const express = require('express')  
+const app = express()  
+const port = 3000
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get('/', (request, response) => {  
+  response.send('Hello from Express!')
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(port, (err) => {  
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${port}`)
 })
